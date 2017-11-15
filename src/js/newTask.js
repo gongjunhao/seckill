31a7726eb4fa8f75c7693bbac476986f71cec259
junhao <gongjunhao@sina.com>
2017-11-15 17:33:41 +0800

    bottom-from init

--- src/js/newTask.js
+++ src/js/newTask.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
+    $.get(chrome.extension.getURL('page/newTask.html'), function(data) {
+        $($.parseHTML(data)).appendTo('body');
+    });
     $("#secKillForm #close").on("click", function () {
         $("#secKillForm").remove();
     })
