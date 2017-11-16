4bfb05a205849c1ae7e98713175d63ab51d57e95
junhao <gongjunhao@sina.com>
2017-11-16 15:43:03 +0800

    bottom-from init

--- src/js/newTask.js
+++ src/js/newTask.js
@@ -1,8 +1,42 @@
-$(document).ready(function () {
-    $("#secKillForm #close").on("click", function () {
+(function() {
+    $("#close").click(function () {
         $("#secKillForm").remove();
     });
-    $("#killTime").click(function () {
-        WdatePicker({el:this,dateFmt:'yyyy-MM-dd HH:mm:ss'});
+    $("#add").click(function () {
+        //
+        // Define your database
+        //
+        var db = new Dexie("friend_database");
+        db.version(1).stores({
+            friends: 'name,shoeSize'
+        });
+
+        //
+        // Put some data into it
+        //
+        db.friends.put({name: "Nicolas", shoeSize: 8}).then (function(){
+            //
+            // Then when data is stored, read from it
+            //
+            return db.friends.get('Nicolas');
+        }).then(function (friend) {
+            //
+            // Display the result
+            //
+            alert ("Nicolas has shoe size " + friend.shoeSize);
+        }).catch(function(error) {
+            //
+            // Finally don't forget to catch any error
+            // that could have happened anywhere in the
+            // code blocks above.
+            //
+            alert ("Ooops: " + error);
+        });
+        // document.getElementById("close").addEventListener("click", function(e) {
+        //     var element = document.getElementById("secKillForm");
+        //     element.outerHTML = "";
+        //     delete element;
+        // }, true);
     });
-});
+})();
+
