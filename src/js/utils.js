9f63855519b5276dd8a9cdc95de64acf351ede16
gongjunhao@sina.com <Gb19870927>
2017-11-18 22:49:55 +0800

    invoke the api get standertime - beijing

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,6 +1,38 @@
+var standerTime = new Date().getTime();
+console.log(standerTime);
 $(document).ready(function () {
 
+    //标题长度限制
     var titleLength = 20;
+
+    //调用接口获取北京时间
+    $.ajax({
+        type          : 'get',
+        async         : false,
+        url           : 'https://sapi.k780.com/?app=life.time&appkey=29705&sign=935228281845e8be2ec7481f13e9beee&format=json&jsoncallback=data',
+        dataType      : 'jsonp',
+        jsonp         : 'callback',
+        jsonpCallback : 'data',
+        success       : function(data){
+            if(data.success!='1'){
+                console.error(data.msgid+' '+data.msg);
+                console.error("调用接口获取北京时间失败！");
+                updateTime();
+                exit;
+            }
+            for(var i in data.result){
+                var property=data.result[i];
+                if(i == "timestamp") {
+                    standerTime = parseInt((parseInt(property)+0)+"500");
+                    updateTime();
+                }
+            }
+        },
+        error:function(){
+            console.error("调用接口获取北京时间失败！");
+            updateTime();
+        }
+    });
 
     //新建任务
     $("#addNewTask").click(function () {
@@ -18,7 +50,8 @@ $(document).ready(function () {
     //加载任务列表数据
     chrome.storage.local.get({"tasks": new Array()}, function(value){
         var tasks = value.tasks;
-        if(tasks.length >0) {
+        console.log(tasks);
+        if(tasks.length > 0) {
             for(var i=0; i<tasks.length; i++) {
                 var card = $("#templateCard").clone();
                 $(card).find(".taskContainer").removeAttr("id");
@@ -28,7 +61,7 @@ $(document).ready(function () {
                 $("#message").after($(card).html());
             }
         } else {
-            $("message").text("暂无秒杀任务，快去添加吧！");
+            $("#message").text("暂无秒杀任务，快去新增吧！");
         }
     });
 
@@ -41,3 +74,34 @@ $(document).ready(function () {
 
 
 });
+
+/**
+ * 更新popup page的北京时间
+ */
+function updateTime() {
+    setInterval(function () {
+        standerTime += 1000;
+        $("#standTime").text(formatDateTime(standerTime));
+    }, 1000);
+}
+
+/**
+ * 日期格式化
+ * @param inputTime
+ * @returns {string}
+ */
+function formatDateTime(inputTime) {
+    var date = new Date(inputTime);
+    var y = date.getFullYear();
+    var m = date.getMonth() + 1;
+    m = m < 10 ? ('0' + m) : m;
+    var d = date.getDate();
+    d = d < 10 ? ('0' + d) : d;
+    var h = date.getHours();
+    h = h < 10 ? ('0' + h) : h;
+    var minute = date.getMinutes();
+    var second = date.getSeconds();
+    minute = minute < 10 ? ('0' + minute) : minute;
+    second = second < 10 ? ('0' + second) : second;
+    return y + '-' + m + '-' + d+' '+h+':'+minute+':'+second;
+}
